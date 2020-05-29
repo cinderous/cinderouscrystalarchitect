77712989e718fb22747b820eedbdede248dd07b0
cinderous <themattchoo@gmail.com>
2020-05-29 06:03:57 -0700

    experiement box facing
    no errors anymore, but block does not face correctly when placed.

--- src/main/java/com/cinderous/crystalarchitect/blocks/ExperimentBox.java
+++ src/main/java/com/cinderous/crystalarchitect/blocks/ExperimentBox.java
@@ -7,6 +7,7 @@ import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.state.DirectionProperty;
+import net.minecraft.state.StateContainer;
 import net.minecraft.util.Direction;
 
 public class ExperimentBox extends Block {
@@ -25,5 +26,10 @@ public class ExperimentBox extends Block {
     @Override
     public BlockState getStateForPlacement(BlockItemUseContext context) {
         return this.getDefaultState().with(FACING, context.getPlacementHorizontalFacing().getOpposite());
+    }
+
+    @Override
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
     }
 }
