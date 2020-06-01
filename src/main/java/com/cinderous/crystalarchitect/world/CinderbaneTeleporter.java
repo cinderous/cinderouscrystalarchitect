e3ce881560c099d939f4734b7af0b74e4efb1334
cinderous <themattchoo@gmail.com>
2020-06-01 04:33:09 -0700

    frsutrated
    frustrated

--- src/main/java/com/cinderous/crystalarchitect/world/CinderbaneTeleporter.java
+++ src/main/java/com/cinderous/crystalarchitect/world/CinderbaneTeleporter.java
@@ -27,8 +27,8 @@ public class CinderbaneTeleporter  implements ITeleporter {
     }
 
 
-    public BlockPos finalpos = new BlockPos(0, 0, 0);
-    public BlockPos freshpos = new BlockPos(0, 0, 0);
+    //public BlockPos finalpos = new BlockPos(0, 0, 0);
+
     public BlockPos testpos;
 
     @Override
@@ -40,6 +40,7 @@ public class CinderbaneTeleporter  implements ITeleporter {
             int heightlevel = destWorld.getHeight();
             int maxX = 200;
             int maxZ = 200;
+            BlockPos startpos = new BlockPos(0,0,0);
 
             Vec3d loc = new Vec3d(0, 0, 0);
 
@@ -49,57 +50,54 @@ public class CinderbaneTeleporter  implements ITeleporter {
                     CrystalArchitect.LOGGER.info(" THE Y LOOP");
                         for (int z = 0; z < maxZ; ++z) {
                             CrystalArchitect.LOGGER.info("THE Z LOOP");
-                            //int index = y * maxX * maxZ + z * maxX + x;
-                            Vec3d testing =  new Vec3d (x, y, z);
+                            CrystalArchitect.LOGGER.info(startpos);
+                            int i = startpos.getY() + 1;
+                            BlockPos freshpos = new BlockPos(x, y, z);
 
-                            freshpos.add(x,y,z);
-                            //testpos.(testing);
-
-                            CrystalArchitect.LOGGER.info(z);
+                            CrystalArchitect.LOGGER.info(freshpos);
+                            //CrystalArchitect.LOGGER.info(z);
 
 
 
                             if (destWorld.getBlockState(freshpos).getBlock() == Blocks.AIR.getDefaultState().getBlock()) {
-                                freshpos.add(0,1,0);
-                                test += 1;
-                                CrystalArchitect.LOGGER.info(test);
+                                int ii = freshpos.getY() + 1;
+                                BlockPos airpos = new BlockPos(x, ii, z);
+
+                                //CrystalArchitect.LOGGER.info("test","airpos.toString()", "test");
 
                                CrystalArchitect.LOGGER.info("FOUND AIR AND ADDING to Y");
                                 //does not appear to be adding to the vector3 loop endless is stuck at y 63
-                                CrystalArchitect.LOGGER.info(freshpos.getY());
+                                //CrystalArchitect.LOGGER.info(airpos.getY());
+                                CrystalArchitect.LOGGER.info(airpos);
 
+                                BlockPos finalpos = new BlockPos(x, airpos.getY(), z);
 
-
-                                finalpos.add(freshpos.getX(), freshpos.getY(), freshpos.getZ());
-
-                                if(checkForSky(heightlevel, destWorld, freshpos) == heightlevel) {
-                                    entity.setPosition(finalpos.getX(), finalpos.getY() + 2, finalpos.getZ());
-                                };
-
-
-
-                                }
-
-
-                                }
+                                checkForSkyAndTeleport(heightlevel, destWorld, freshpos, finalpos, entity) ;
+                            }
                         }
+                    }
 
-                    }return repositionEntity.apply(false);
-                }
+                }return repositionEntity.apply(false);
+    }
 
-                public int checkForSky(int skybox, ServerWorld dest, BlockPos skycheckpos) {
-                    while( dest.getBlockState(skycheckpos)==Blocks.AIR.getDefaultState() && skycheckpos.getY()<=skybox ){
-                        skycheckpos.add(0,1,0);
-                        CrystalArchitect.LOGGER.info(skycheckpos.getY());
-                        finalpos.add(0, skycheckpos.getY(),0);
-                        //does not appear to be adding to the vector3 loop endless is stuck at y 63
+    public boolean checkForSkyAndTeleport(int skybox, ServerWorld dest, BlockPos skycheckpos, BlockPos finalpos, Entity entity) {
+        while( dest.getBlockState(skycheckpos)==Blocks.AIR.getDefaultState() && skycheckpos.getY()<=skybox ){
+            int ii = skycheckpos.getY() + 1;
+            BlockPos upwardspos = new BlockPos(skycheckpos.getX(), ii, skycheckpos.getZ());
 
-                    } return skycheckpos.getY();
+            CrystalArchitect.LOGGER.info(skycheckpos.getY());
+
+            int iii = finalpos.getY() + 1;
+            BlockPos readypos = new BlockPos(finalpos.getX(), iii,finalpos.getZ());
+
+            entity.setPosition(readypos.getX(), readypos.getY() + 2, readypos.getZ());
+
+        } return true;
 
 
-                }
+    }
 
-        }
+}
 
 
 
